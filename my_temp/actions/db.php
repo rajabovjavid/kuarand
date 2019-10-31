d662f02fa12297477fad2ce57b519254fc5c80e7
javidrajabov <jvdrjbv@gmail.com>
2019-11-01 01:10:42 +0300

    remote mysql hosting is added

--- my_temp/actions/db.php
+++ my_temp/actions/db.php
@@ -2,7 +2,7 @@
 
 try {
 
-    $db = new PDO("mysql:host=localhost;dbname=randevu;charset=utf8", 'root', 'mysql');
+    $db = new PDO("mysql:host=remotemysql.com;dbname=faYpJ2XCps;charset=utf8", 'faYpJ2XCps', 'Xe0ZbhAxe4');
     //echo "veritabanı bağlantısı başarılı";
 } catch (PDOExpception $e) {
     echo $e->getMessage();
