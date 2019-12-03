f62d6218de3e2f804da02db1bd54fa322c0ace91
javidrajabov <jvdrjbv@gmail.com>
2019-12-03 14:15:49 +0300

    aws db is connected

--- my_temp/actions/db.php
+++ my_temp/actions/db.php
@@ -2,8 +2,8 @@
 
 try {
 
-    $db = new PDO("mysql:host=remotemysql.com;dbname=faYpJ2XCps;charset=utf8", 'faYpJ2XCps', 'Xe0ZbhAxe4');
-    //echo "veritabanı bağlantısı başarılı";
+    $db = new mysqli("jvdrjbvaws.cffb5ecxegu8.us-east-2.rds.amazonaws.com", "kuarandawslogin", "kuafofrandevuaws", "kuarand");
+    echo "veritabanı bağlantısı başarılı";
 } catch (PDOExpception $e) {
     echo $e->getMessage();
 }
