3fac91bb5beb6d788e2d60de591569ed96e69d86
rajabovjavid <jvdrjbv@gmail.com>
2020-01-26 12:18:25 +0300

    1) hd_info sayfası güncellendi, daha düzenli ve readonly özelliği bazı kısımlara eklendi, 2) update_hd_route.php'da her şey direct post'dan alınıyor apc yerine

--- my_temp/api_routes/panel_routes/update_hd_route.php
+++ my_temp/api_routes/panel_routes/update_hd_route.php
@@ -2,40 +2,26 @@
 ob_start();
 session_start();
 
-if(apcu_fetch("panel_type")=="admin"){ // kuaför güncelleme isteği admin tarafından gelmişse
-    exit;
+include "../curl_api.php";
+
+/*if(apcu_fetch("panel_type")=="admin"){ // kuaför güncelleme isteği admin tarafından gelmişse
+
 }
 elseif (apcu_fetch("panel_type")=="hairdresser"){ // kuaför kendi bilgilerini güncellemek istiyorsa
-    $hd = apcu_fetch("user_data");
-
-    $hdName = $_POST["hdName"]; // kuaför ismini formdan alıyoruz
-    if($hdName==""){// eğer ismi boş göndermişse, apc'deki ismi kullanıyoruz
-        $hdName = $hd["hdName"];
-    }
-
-    $hdEmail = $hd["hdEmail"]; // emaili güncelliyemeyeceği için, apc'de tutduğumuz user data'dan alıyoruz
-
-    $hdPassword = $_POST["hdPassword"]; // kuaför şifresini formdan alıyoruz
-    if($hdPassword==""){ // eğer şifreyi boş göndermişse, apc'deki şifreyi kullanıyoruz
-        $hdPassword = $hd["hdPassword"];
-    }
-
-    $hdType = $_POST["hdType"]; // kuaför type'ını formdan alıyoruz
-
-    $hdStatus = $hd["hdStatus"]; // kuaför status'unu formdan güncelliyemiyor zaten, ondan dolayı apc'deki status alıyoruz
-}
+    // TODO - hdName, hdPassword, hdType en az bir tanesini değiştirmemişse, api call yapılmadan geri dönülmesi lazım
+}*/
 
 // api'ye istek için post datası
 $data_array = array(
-    "hd_name" => $hdName,
-    "hd_email" => $hdEmail,
-    "hd_password" => $hdPassword,
-    "hd_type" => $hdType,
-    "hd_status" => $hdStatus
+    "hd_name" => $_POST["hdName"],
+    "hd_email" => $_POST["hdEmail"],
+    "hd_password" => $_POST["hdPassword"],
+    "hd_type" => $_POST["hdType"],
+    "hd_status" => $_POST["hdStatus"]
 );
 
 // api'ye istek atma
-$make_call = callAPI('POST', 'http://localhost/rest_api_slim/public/api/hairdresser/updateHairdresser', json_encode($data_array));
+$make_call = callAPI('PUT', 'http://localhost/rest_api_slim/public/api/hairdresser/updateHairdresser', json_encode($data_array));
 $response = json_decode($make_call, true); // api'den gelen response
 $status = $response["status"];
 $message = $response["message"];
@@ -43,17 +29,19 @@ $message = $response["message"];
 if($status == null) {
     apcu_store("action_status", $status);
     apcu_store("message", "sistem hatası");
-    header("Location:../../nedmin/production/kuafor_bilgileri.php");
+    header("Location:../../nedmin/production/hd_info.php");
     exit;
 }
 elseif ($status == "error"){
     apcu_store("action_status", $status);
     apcu_store("message", $message);
-    header("Location:../../nedmin/production/kuafor_bilgileri.php");
+    header("Location:../../nedmin/production/hd_info.php");
     exit;
 }
 elseif ($status == "ok"){
-    apcu_delete("user_data");
-
+    apcu_store("action_status", $status);
+    apcu_store("message", $message);
+    apcu_store("user_data", $response["data"]);
+    header("Location:../../nedmin/production/hd_info.php");
 }
 
---
+++ my_temp/nedmin/production/hd_info.php
@@ -0,0 +1,149 @@
+<?php
+
+include 'header.php';
+
+?>
+
+
+<!-- page content -->
+<div class="right_col" role="main">
+    <div class="">
+
+        <div class="clearfix"></div>
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                    <div class="x_title">
+                        <h2>Kuaför Bilgileri
+                            <small>
+
+                                <b style="color:<?php echo (apc_fetch("action_status") == "ok") ? 'green' : 'red' ?>;">
+                                    <?php
+                                    echo apc_fetch("message");
+                                    apcu_delete("message");
+                                    ?>
+                                </b>
+
+                            </small>
+                        </h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <br/>
+
+                        <!-- / => en kök dizine çık ... ../ bir üst dizine çık -->
+                        <form action="../../api_routes/panel_routes/update_hd_route.php" method="POST" id="demo-form2"
+                              data-parsley-validate
+                              class="form-horizontal form-label-left">
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Kuaför İsmi
+                                    <span class="required">*</span>
+                                </label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <input type="text" id="first-name" name="hdName"
+                                           value="<?php echo $user_data['hdName'] ?>" required="required"
+                                           class="form-control col-md-7 col-xs-12">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Kuaför Şifre
+                                    <span
+                                            class="required">*</span>
+                                </label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <input type="password" id="first-name" name="hdPassword"
+                                           placeholder="şifrenizi güncellemek istemiyorsanız boş bırakın"
+                                           value=""
+                                           class="form-control col-md-7 col-xs-12">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Kuaför
+                                    Email</label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <input type="text" id="first-name" name="hdEmail" readonly
+                                           value="<?php echo $user_data['hdEmail'] ?>" required="required"
+                                           class="form-control col-md-7 col-xs-12">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Kuaför Type
+                                    <span
+                                            class="required">*</span>
+                                </label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <select id="first" class="form-control" name="hdType" required="">
+                                        <option value="0" <?php if ($user_data['hdType'] == 0) echo 'selected="selected"'; ?>>
+                                            Kadın Kuaförü
+                                        </option>
+                                        <option value="1" <?php if ($user_data['hdType'] == 1) echo 'selected="selected"'; ?>>
+                                            Erkek Kuaförü
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Kuaför
+                                    Durum</label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <input type="text" id="first-name" name="status_text" readonly
+                                           value="<?php
+                                           if ($user_data['hdStatus'] == "0") echo 'Kayıt talebiniz inceleniyor';
+                                           elseif ($user_data['hdStatus'] == "-1") echo 'Kayıt talebiniz onaylanmadı';
+                                           elseif ($user_data['hdStatus'] == "1") echo 'Kayıtınız onaylandı, Kuaför panelinde tüm yetkilere sahipsiniz';
+                                           elseif ($user_data['hdStatus'] == "2") echo 'Siteye alınma talebiniz inceleniyor';
+                                           elseif ($user_data['hdStatus'] == "-2") echo 'Siteye alınma talebiniz onaylanmadı';
+                                           elseif ($user_data['hdStatus'] == "3") echo 'Siteye alınma talebiniz onaylandı, sitede aktif durumdasınız';
+                                           ?>"
+                                           required="required"
+                                           class="form-control col-md-7 col-xs-12">
+                                    <input type="hidden" name="hdStatus" value="<?php echo $user_data['hdStatus'];?>">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Kuaför
+                                    Puanı</label>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <input type="text" id="first-name" name="rating" readonly
+                                           value="<?php echo $user_data['hdRating'] ?>" required="required"
+                                           class="form-control col-md-7 col-xs-12">
+                                </div>
+                            </div>
+
+
+                            <div class="ln_solid"></div>
+                            <div class="form-group">
+                                <div align="right" class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                    <button type="submit" name="updateHairdresser" class="btn btn-success">Güncelle
+                                    </button>
+                                </div>
+                            </div>
+
+                        </form>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <hr>
+        <hr>
+        <hr>
+
+
+    </div>
+</div>
+<!-- /page content -->
+
+<?php include 'footer.php'; ?>
